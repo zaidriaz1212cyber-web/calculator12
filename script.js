e993748c4a748c15622000eba6eef594f358a774
zaid <zaidriaz1212cyber@gmail.com>
2026-01-11 19:02:23 +0500

    calculator

---
+++ script.js
@@ -0,0 +1,17 @@
+var display = document.getElementById("display");
+
+function append(value) {
+  display.value += value;
+}
+
+function clearDisplay() {
+  display.value = "";
+}
+
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch {
+    display.value = "Error";
+  }
+}
